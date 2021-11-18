27005ffb72c523ba02b9fd4557f3eaacf9a66e3e
anjnakS <66026895+anjnakS@users.noreply.github.com>
2021-11-17 18:59:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    Hello {value}
+  <br>
+  <tr>
+
+                <td><a href= "#">{"http://www.google.com"}</a></td>
+              </tr>
+  </div>;
 }
 
 export default App;
