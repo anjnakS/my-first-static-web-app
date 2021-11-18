43d14e2e971df30276eec3b61164f8276c146956
anjnakS <66026895+anjnakS@users.noreply.github.com>
2021-11-17 19:04:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>
-    Hello {value}
-  <br>
-  <tr>
-
-                <td><a href= "#">{"http://www.google.com"}</a></td>
-              </tr>
-  </div>;
+  return <div>Hello {value} Create a button to take you to client.</div>;
 }
 
 export default App;
